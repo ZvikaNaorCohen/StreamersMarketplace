db02951e534a31d0026e9cdbc0a2cb25ec27c1d8
Zvi Naor Cohen <zvinaorco@mta.ac.il>
2023-04-05 16:28:49 +0300

    Adding hero, Card, Carusela, and updating App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,9 @@
 // import Header from './components/Header'
 import React from 'react';
 import Navbar from './components/Navbar'
+import Carusela from "./components/Carusela";
 import Footer from './components/Footer'
+import Hero from './components/Hero'
 // import { Routes, Route, useLocation} from 'react-router-dom';
 // import Navigation from './pages/Navbar';
 // import Explore from './pages/Explore';
@@ -17,10 +19,54 @@ import Footer from './components/Footer'
     </Route>
 */
 
+/*
+  THIS CODE IS JUST FOR TESTING, NEED TO DELETE. IT WILL SHOW US HOW CARDS AND CARUSELA WORK
+*/
+const cards = [
+  {
+    title: "Card 1",
+    image: "https://images.pexels.com/photos/14905759/pexels-photo-14905759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    title: "Card 2",
+    image: "https://images.pexels.com/photos/14343389/pexels-photo-14343389.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "Card 3",
+    image: "https://images.pexels.com/photos/15636474/pexels-photo-15636474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: "Card 4",
+    image: "https://images.pexels.com/photos/15140108/pexels-photo-15140108.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: "Card 5",
+    image: "https://images.pexels.com/photos/15784886/pexels-photo-15784886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: "Card 6",
+    image: "https://images.pexels.com/photos/16083380/pexels-photo-16083380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
+
+
+
 function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
+      <Carusela title="Trending" cards={cards} />
+      <div className="carusela-spacing" />
+      <Carusela title="New On Site" cards={cards} />
+      <div className="carusela-spacing" />
+      <Carusela title="Don't Miss!" cards={cards} />
       <Footer/>
     </>
   );
---
+++ my-app/src/components/Carusela.js
@@ -0,0 +1,85 @@
+import React from "react";
+import Slider from "react-slick";
+import Card from "./Card";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Carusela.css"
+
+
+const Carusela = ({ title, cards }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={`${className} carusela__arrow carusela__arrow--next`}
+        style={{ ...style, display: "block" }}
+        onClick={onClick}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+          <path d="M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z" />
+        </svg>
+      </div>
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={`${className} carusela__arrow carusela__arrow--prev`}
+        style={{ ...style, display: "block" }}
+        onClick={onClick}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+          <path d="M15.41 7.41L10.83 12L15.41 16.59L14 18L8 12L14 6L15.41 7.41Z" />
+        </svg>
+      </div>
+    );
+  }
+
+  return (
+    <div className="carusela">
+      <h2 className="carusela__title">{title}</h2>
+      <div className="carusela__slider">
+        <Slider {...settings}>
+          {cards.map((card, index) => (
+            <div key={index}>
+              <Card {...card} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Carusela;
