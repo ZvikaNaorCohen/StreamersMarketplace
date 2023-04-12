cdf6dfe0bc436bb1fe2c18bcdb28d614c4118431
Zvi Naor Cohen <zvinaorco@mta.ac.il>
2023-04-12 17:14:29 +0300

    Added login with metamask

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,7 +1,22 @@
-import React from 'react';
 import './Navbar.css';
+import React, { useState } from 'react';
+import Web3 from 'web3';
 
 function Navbar() {
+  const web3 = new Web3(window.ethereum);
+
+  async function metamaskButtonHandler() {
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+      // Get user account address
+      const accounts = await web3.eth.getAccounts();
+      alert(`Logged in with account: ${accounts[0]}`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -9,8 +24,13 @@ function Navbar() {
       </div>
       <div className="navbar-search">
         <input type="text" placeholder="Search" className="navbar-search-input" />
-    </div>
+      </div>
       <ul className="navbar-links">
+        <li>
+          <button className="metamask-button" onClick={metamaskButtonHandler}>
+            Metamask
+          </button>
+        </li>
         <li><button className="navbar-button">Home</button></li>
         <li><button className="navbar-button">About</button></li>
         <li><button className="navbar-button">Explore</button></li>
