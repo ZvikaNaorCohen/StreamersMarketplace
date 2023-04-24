11f826a689f80276964b788e878380d94887b748
Matan Cohen <matan10cohen@gmail.com>
2023-04-24 20:46:56 +0300

    Init NFT page (Item) with item obj

--- my-app/src/components/NFTHeader.js
+++ my-app/src/components/NFTHeader.js
@@ -1,128 +1,8 @@
-// import React from "react";
-// import styled from "styled-components";
-// import Cards from "./NFTCards";
-// import Details from "./NFTDetails";
-// import Pricing from "./NFTPrice";
-// import NFTImg from "../images/NFTpageImages/doge.jpg";
-
-// const Left = styled.div`
-//   flex: 2;
-//   display: flex;
-//   animation: animate 2s infinite ease alternate;
-//   max-width: 600px;
-
-//   @keyframes animate {
-//     to {
-//       transform: translateY(5px);
-//     }
-//   }
-// `;
-
-// const Right = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: Left;
-//   position: relative;
-//   background-color: white;
-//   flex: 3;
-//   max-width: 800px;
-//   max-height: 600px;
-//   gap: 5px;
-//   margin: 2rem;
-//   padding-top: 10px;
-// `;
-
-// const Container = styled.div`
-//   display: flex;
-//   flex-direction: row;
-// `;
-
-// const NFTHeader = () => {
-//   return (
-//     <Container>
-//       <Left>
-//         <Cards title={"Doge"} image={NFTImg} />
-//       </Left>
-//       <Right>
-//         <Details
-//           NFTName={"Doge"}
-//           creatorName={"someone"}
-//           NFTDescription={"This is a description about a very special doge."}
-//         />
-//         <Pricing Price={"0.35"}/>
-//       </Right>
-//     </Container>
-//   );
-// };
-
-// export default NFTHeader;
-
-// works up
-
-// import React from "react";
-// import styled from "styled-components";
-// import Cards from "./NFTCards";
-// import Details from "./NFTDetails";
-// import Pricing from "./NFTPrice";
-
-// const Left = styled.div`
-//   flex: 2;
-//   display: flex;
-//   animation: animate 2s infinite ease alternate;
-//   max-width: 600px;
-
-//   @keyframes animate {
-//     to {
-//       transform: translateY(5px);
-//     }
-//   }
-// `;
-
-// const Right = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: left;
-//   position: relative;
-//   background-color: white;
-//   flex: 3;
-//   max-width: 800px;
-//   max-height: 600px;
-//   gap: 5px;
-//   margin: 2rem;
-//   padding-top: 10px;
-// `;
-
-// const Container = styled.div`
-//   display: flex;
-//   flex-direction: row;
-// `;
-
-// const NFTHeader = ({ title, image, creatorName, NFTDescription, price }) => {
-//   return (
-//     <Container>
-//       <Left>
-//         <Cards title={title} image={image} />
-//       </Left>
-//       <Right>
-//         <Details
-//           NFTName={title}
-//           creatorName={creatorName}
-//           NFTDescription={NFTDescription}
-//         />
-//         <Pricing Price={price} />
-//       </Right>
-//     </Container>
-//   );
-// };
-
-// export default NFTHeader;
-
 import React from "react";
 import styled from "styled-components";
 import Cards from "./NFTCards";
 import Details from "./NFTDetails";
 import Pricing from "./NFTPrice";
-import NFTImg from "../images/NFTpageImages/doge.jpg";
 
 const Left = styled.div`
   flex: 2;
@@ -156,19 +36,19 @@ const Container = styled.div`
   flex-direction: row;
 `;
 
-const NFTHeader = () => {
+const NFTHeader = ({ title, image, creatorName, NFTDescription, Price }) => {
   return (
     <Container>
       <Left>
-        <Cards title={"Title"} image={NFTImg} />
+        <Cards title={title} image={image} />
       </Left>
       <Right>
         <Details
-          NFTName={"Title"}
-          creatorName={"creatorName"}
-          NFTDescription={"This is a description about a very special doge."}
+          NFTName={title}
+          creatorName={creatorName}
+          NFTDescription={NFTDescription}
         />
-        <Pricing Price={"0.35"} />
+        <Pricing Price={Price} />
       </Right>
     </Container>
   );
--- my-app/src/pages/Item.js
+++ my-app/src/pages/Item.js
@@ -1,74 +1,29 @@
-// import React from 'react';
-// import { useParams } from "react-router-dom";
-// import NFTpage from '../components/NFTpage';
-
-// function Item() {
-//   var slug = useParams();
-//   return (
-//     <>
-//       <NFTpage id={slug} />
-//     </>
-//   );
-// }
-
-// export default Item;
-
-// works up
-
-// import React from "react";
-// import { useParams } from "react-router-dom";
-// import { Routes, Route } from "react-router-dom";
-// import NFTHeader from "../components/NFTHeader";
-
-// const items = [
-//   {
-//     title: "Doge",
-//     image: "../images/NFTpageImages/doge.jpg",
-//     creatorName: "Someone",
-//     NFTDescription: "This is a description about a very special doge.",
-//     price: "0.35",
-//   },
-// ];
-
-// function Item() {
-//   const { id } = useParams();
-//   const item = items[id];
-//   return (
-//     <>
-//       <NFTHeader
-//         title={item.title}
-//         image={item.image}
-//         creatorName={item.creatorName}
-//         NFTDescription={item.NFTDescription}
-//         price={item.price}
-//       />
-//     </>
-//   );
-// }
-
-// export default Item;
-
 import React from "react";
 import { useParams } from "react-router-dom";
 import "../App.css";
 import NFTHeader from "../components/NFTHeader";
+import NFTImg from "../images/NFTpageImages/doge.jpg";
 
-let item = [
-  {
-    title: "Doge",
-    image: "../images/NFTpageImages/doge.jpg",
-    creatorName: "Someone",
-    NFTDescription: "This is a description about a very special doge.",
-    Price: "0.35",
-  },
-];
+const item = {
+  title: "Doge",
+  image: NFTImg,
+  creatorName: "Someone",
+  NFTDescription: "This is a description about a very special doge.",
+  Price: "0.35",
+};
 
 function Item() {
-  var slug = useParams();
+  const { id } = useParams();
+
   return (
     <>
-      <NFTHeader/>
-       {/* <NFTCarusela /> */}
+      <NFTHeader
+        title={item.title}
+        image={item.image}
+        creatorName={item.creatorName}
+        NFTDescription={item.NFTDescription}
+        Price={item.Price}
+      />
     </>
   );
 }
