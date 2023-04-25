67bf1a3c653170560c670af8ef19e4eee8330dad
Matan Cohen <matan10cohen@gmail.com>
2023-04-25 16:16:50 +0300

    Added a working "Sign in with Google" button

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,10 +1,45 @@
 import './Navbar.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
 import Web3 from 'web3';
-import newLogo from '../images/NewLogo.png'
+import newLogo from '../images/NewLogo.png';
+import axios from "axios";
 
 function Navbar() {
   const web3 = new Web3(window.ethereum);
+
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
 
   async function metamaskButtonHandler() {
     try {
@@ -21,10 +56,16 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <a href="/"><img src={newLogo} className="AppNewLogo" alt="logo" /></a>
+        <a href="/">
+          <img src={newLogo} className="AppNewLogo" alt="logo" />
+        </a>
       </div>
       <div className="navbar-search">
-        <input type="text" placeholder="Search" className="navbar-search-input" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="navbar-search-input"
+        />
       </div>
       <ul className="navbar-links">
         <li>
@@ -32,11 +73,37 @@ function Navbar() {
             Metamask
           </button>
         </li>
-        <li><button className="navbar-button">Home</button></li>
-        <li><button className="navbar-button">About</button></li>
-        <li><button className="navbar-button">Explore</button></li>
-        <li><button className="navbar-button">Contact</button></li>
-        <li><button className="navbar-button">Login</button></li>
+        <li>
+          <button className="navbar-button">Home</button>
+        </li>
+        <li>
+          <button className="navbar-button">About</button>
+        </li>
+        <li>
+          <button className="navbar-button">Explore</button>
+        </li>
+        <li>
+          <button className="navbar-button">Contact</button>
+        </li>
+        {profile ? (
+          <div className="navbar-login">
+            <img src={profile.picture} alt="user image" />
+            <h3>User Logged in</h3>
+            <p>Name: {profile.name}</p>
+            <p>Email Address: {profile.email}</p>
+            <br />
+            <br />
+            <button onClick={logOut}>Log out</button>
+          </div>
+        ) : (
+          <button className="navbar-login-button" onClick={() => login()}>
+            Sign in with Google{" "}
+          </button>
+        )}
+
+        {/* <GoogleLogin onSuccess={responseMessage} onError={errorMessage} /> */}
+
+        {/* <li><button className="navbar-button">Login</button></li> */}
       </ul>
     </nav>
   );
