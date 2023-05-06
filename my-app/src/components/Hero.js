bddc5b69e956f07930149e0019ef4dd33f9b5d77
Matan Cohen <matan10cohen@gmail.com>
2023-05-06 18:23:01 +0300

    Added sticky navbar

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -8,9 +8,7 @@ const Hero = () => {
         <h1>Welcome to My Site</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet elit eu elit lacinia ultricies. Etiam
-          rhoncus, est at suscipit aliquam, turpis lorem gravida eros, in hendrerit lectus ante sit amet dolor. Sed
-          malesuada odio ut est ultrices posuere. Vestibulum tempus posuere quam sed facilisis. Integer ac felis
-          fermentum, suscipit dolor eget, suscipit urna.
+          rhoncus, est at suscipit aliquam, turpis lorem gravida eros, in hendrerit lectus ante sit amet dolor.
         </p>
         <button>Learn More</button>
       </div>
