dd7365fcc85b620f1162c546e56937333673d2c0
Zvi Naor Cohen <zvinaorco@mta.ac.il>
2023-05-13 16:45:33 +0300

    Added stream site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import CreatorProfile from "./pages/CreatorProfile";
 import Item from "./pages/Item";
 import Contact from "./pages/Contact";
+import CreatorStream from "./components/CreatorStream"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/item/:slug" element={<Item />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/stream" element={<CreatorStream/>} />
       </Routes>
       <Footer />
     </>
