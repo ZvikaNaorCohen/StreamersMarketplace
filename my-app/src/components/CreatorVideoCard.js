4bd85331d11f651efc2749ae97f9f6979a45ad87
Zvi Naor Cohen <zvika96zvika@gmail.com>
2023-05-29 23:12:55 +0300

    Added random image to view all videos

--- my-app/src/components/CreatorVideoCard.js
+++ my-app/src/components/CreatorVideoCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CreatorVideoCard.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,7 +11,9 @@ function CreatorVideoCard(props) {
 
   return (
     <div className="card">
-      <div className="card-image"></div>
+      <div className="card-image">
+        <img src={'https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80'} />
+      </div>
       <div className="card-content">
         <h3>{props.item.body.title}</h3>
         <div className="card-footer">
